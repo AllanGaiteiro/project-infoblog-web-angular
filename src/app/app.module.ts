5ce5e492909ac037cab64087ef4f1261038640ba
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-05-04 15:01:19 -0300

    feat: gerar a base do projeto infoblog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { FooterBlogComponent } from './components/footer-blog/footer-blog.component';
+import { HeaderBlogComponent } from './components/header-blog/header-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    FooterBlogComponent,
+    HeaderBlogComponent
   ],
   imports: [
     BrowserModule
