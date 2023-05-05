8251dcba4b9fc11018a631ba78f8821362d8c72a
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-05-05 16:16:04 -0300

    feat: adcionado uma secton small card list para cada sections

--- src/app/components/setion-small-card-list/setion-small-card-list.component.ts
+++ src/app/components/setion-small-card-list/setion-small-card-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Section } from 'src/app/core/models/section.interface';
 
 @Component({
   selector: 'app-setion-small-card-list',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./setion-small-card-list.component.css']
 })
 export class SetionSmallCardListComponent {
-
+  @Input() section?: Section;
 }
--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Section } from 'src/app/core/models/section.interface';
+import { SectionsService } from 'src/app/services/sections.service';
 
 @Component({
   templateUrl: './page-home.component.html',
   styleUrls: ['./page-home.component.css']
 })
 export class PageHomeComponent {
+  sections?: Section[];
 
+  constructor(private sectionsService: SectionsService) {
+
+  }
+  ngOnInit() {
+    this.findSections();
+  }
+
+  findSections() {
+    this.sectionsService.findAll().then(res => {
+      this.sections = res;
+    })
+  }
 }
