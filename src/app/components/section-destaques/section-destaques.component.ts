5c7dd974fcb1ec345b1fa8f0431153e9a1abb328
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-05-05 00:39:42 -0300

    feat: criado o componente de section destaques usando dados estaticos

---
+++ src/app/components/section-destaques/section-destaques.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-destaques',
+  templateUrl: './section-destaques.component.html',
+  styleUrls: ['./section-destaques.component.css']
+})
+export class SectionDestaquesComponent {
+
+}
