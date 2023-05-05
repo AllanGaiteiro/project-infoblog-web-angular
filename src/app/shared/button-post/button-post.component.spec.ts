8a06b1f69c4a6cf744960e2ee0a2ae8e9cfcc4ec
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-05-04 23:39:58 -0300

    feat: criado o componente button post

---
+++ src/app/shared/button-post/button-post.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonPostComponent } from './button-post.component';
+
+describe('ButtonPostComponent', () => {
+  let component: ButtonPostComponent;
+  let fixture: ComponentFixture<ButtonPostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ButtonPostComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ButtonPostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
