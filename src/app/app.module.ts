00d4d7530c75ebf5f62268b8c9670fa77328fee9
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-05-05 00:40:58 -0300

    feat: import os componente reacionados com o section destaques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 import { FooterBlogComponent } from './components/footer-blog/footer-blog.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { SectionDestaquesComponent } from './components/section-destaques/section-destaques.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { ButtonPostComponent } from './shared/button-post/button-post.component';
+import { CardComponent } from './shared/card/card.component';
 import { LogoComponent } from './shared/logo/logo.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { SearchBarComponent } from './shared/search-bar/search-bar.component';
@@ -17,11 +20,14 @@ import { SearchBarComponent } from './shared/search-bar/search-bar.component';
     BigCardComponent,
     SmallCardComponent,
     FooterBlogComponent,
+    SectionDestaquesComponent,
     LogoComponent,
     MenuComponent,
     SearchBarComponent,
     ButtonPostComponent,
-    NavigationComponent
+    NavigationComponent,
+    CardComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule
