11b377f0652371ec6baf27f98ab5cc17ab02cb1c
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-05-05 21:14:24 -0300

    #1 feat: adcionado uma parametro para capturar o id passado na pagina do post

--- src/app/pages/page-post/page-post.component.ts
+++ src/app/pages/page-post/page-post.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './page-post.component.html',
   styleUrls: ['./page-post.component.css']
 })
 export class PagePostComponent {
+  text?: string;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.text = params['id'];
+    });
+  }
 }
